77fb2032295c5bd743a8db9dab11bfb3c8dc486c
Milos <kenguruubre@gmail.com>
2021-09-27 20:42:29 +0200

    From laptop

--- RxExtensions/Program.cs
+++ RxExtensions/Program.cs
@@ -1,5 +1,7 @@
 ﻿using RxCookbook;
 using System;
+using System.Reactive.Linq;
+using System.Threading;
 
 namespace RxExtensions
 {
@@ -7,22 +9,63 @@ namespace RxExtensions
     {
         static void Main(string[] args)
         {
-            var Dave = new Person();
+            //var stream = Publisher.ObserveSeconds;
+            var observer = Publisher.ObserveSeconds.Subscribe(x => WriteLineColored($"{x}", ConsoleColor.Yellow));
+            Thread.Sleep(5000);
+            var observer2 = Publisher.ObserveSeconds.Subscribe(x => WriteLineColored($"{x}", ConsoleColor.Green));
+            //var observer2 = Publisher.ObserveSeconds.Replay();
+            //observer2.Connect();
+            //observer2.Subscribe(x => WriteLineColored($"{x}", ConsoleColor.Green));
+            //var period = TimeSpan.FromSeconds(1);
+            //var hot = Observable.Interval(period)
+            //.Take(3)
+            //.Publish();
+            //hot.Connect();
+            //Thread.Sleep(period); //Run hot and ensure a value is lost.
+            //var observable = hot.Replay();
+            //observable.Connect();
+            //observable.Subscribe(i => Console.WriteLine("first subscription : {0}", i));
+            //Thread.Sleep(period);
+            //observable.Subscribe(i => Console.WriteLine("second subscription : {0}", i));
+            //Console.ReadKey();
+            //observable.Subscribe(i => Console.WriteLine("third subscription : {0}", i));
+            //Console.ReadKey();
 
-            Dave.ObservePropertyChangedName().Dump("ObservePropertyChangedName");
-            Dave.ObservePropertyChangedObject().Dump("ObservePropertyChangedObject");
-            Dave.ObservePropertyChangedValue(d => d.Name).Dump("ObservePropertyChangedValue");            
-            Dave.ObservePropertyChanged().Dump("ObservePropertyChanged");
+            //var hot = Observable.Interval(TimeSpan.FromSeconds(2)).Take(10).Publish();
+            //hot.Connect();
 
-            //Dave.ObservePropertyChangedName().Merge(Dave.ObservePropertyChangedName()).Dump("PropertyChangedName");
+            //var observable = hot.Replay();
 
-            Dave.Name = "Dave";
-            Dave.Age = 21;
-            Console.WriteLine("hello");
+
+            ////var observer1 = hot.Subscribe(x => WriteLineColored($"Observer {x}", ConsoleColor.Red));
+            //Thread.Sleep(5000);
+
+            //observable.Connect();
+            //observable.Subscribe(x => WriteLineColored($"Observer {x}", ConsoleColor.Green));
+
+
+            //var Dave = new Person();
+
+            //Dave.ObservePropertyChangedName().Dump("ObservePropertyChangedName");
+            //Dave.ObservePropertyChangedObject().Dump("ObservePropertyChangedObject");
+            //Dave.ObservePropertyChangedValue(d => d.Name).Dump("ObservePropertyChangedValue");            
+            //Dave.ObservePropertyChanged().Dump("ObservePropertyChanged");
+
+            ////Dave.ObservePropertyChangedName().Merge(Dave.ObservePropertyChangedName()).Dump("PropertyChangedName");
+
+            //Dave.Name = "Dave";
+            //Dave.Age = 21;
+            Console.WriteLine("Ending...");
 
 
 
             Console.ReadKey();
+        }
+
+        static void WriteLineColored(string message, ConsoleColor color) 
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
         }
     }
 }
---
+++ RxExtensions/Publisher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Disposables;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RxExtensions
+{
+    public static class Publisher
+    {
+        public static IObservable<long> ObserveSeconds { get; }
+
+        static Publisher() 
+        {
+            ObserveSeconds = Observable.Create<long>(observer =>
+            {
+                Task.Run(async () =>
+                {
+                    Console.WriteLine("Observable started");
+                    for (int i = 1; i < 11; i++) 
+                    {
+                        await Task.Delay(2000);
+                        observer.OnNext(i);
+                    }
+                    
+                    //Enumerable.Range(1, 10).ToList().ForEach(async x =>
+                    //{
+                    //    await Task.Delay(2000);
+                    //    observer.OnNext(x);
+                    //});
+                });
+                
+
+                return Disposable.Empty;
+            });
+        }
+    }
+}
